6fe61a6390c1c81d3080442477779ae01913924e
Arman Habib <armanhabib353@gmail.com>
2024-09-21 07:35:20 +0600

    Create splash Screen

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:ecommerce/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  // Todo: Add WIdgets Binding
+  // Todo: Init Local Storage
+  // Todo: Await Native Splash
+  // Todo: Initialize Firebase
+  // Todo: Initialize Authentication
+
+  runApp(const App());
+}
