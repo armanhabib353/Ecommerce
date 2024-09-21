ba88b899882832ccddd5c86fa0cb22d32540adea
Arman Habib <armanhabib353@gmail.com>
2024-09-21 16:48:48 +0600

    Login Screen Making

--- lib/features/autentication/controllers/onboarding/onboarding_controller.dart
+++ lib/features/autentication/controllers/onboarding/onboarding_controller.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce/features/autentication/screens/login/login.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
@@ -20,7 +21,7 @@ class OnboardingController extends GetxController {
   //Todo: Update current Index & Jump to next page
   void nextPage() {
     if (currentPageIndex.value == 2) {
-      // Get.to(LoginScreen());
+      Get.offAll(const LoginScreen());
     } else {
       int page = currentPageIndex.value + 1;
       pageController.jumpToPage(page);
---
+++ lib/features/autentication/screens/login/widgets/login_form.dart
@@ -0,0 +1,81 @@
+import 'package:ecommerce/utils/constants/sizes.dart';
+import 'package:ecommerce/utils/constants/text_strings.dart';
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+class TLoginForm extends StatelessWidget {
+  const TLoginForm({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: TSizes.spaceBtwSections),
+        child: Column(
+          children: [
+            //Todo: Email
+            TextFormField(
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Iconsax.direct_right),
+                labelText: TTexts.email,
+              ),
+            ),
+
+            const SizedBox(height: TSizes.spaceBtwInputFields),
+
+            //Todo: Password
+            TextFormField(
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Iconsax.password_check),
+                labelText: TTexts.password,
+                suffixIcon: Icon(Iconsax.eye_slash),
+              ),
+            ),
+
+            const SizedBox(height: TSizes.spaceBtwInputFields / 2),
+
+            //Todo: Remember Me & Forget Password
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                //Todo: Remember me
+                Row(
+                  children: [
+                    Checkbox(value: true, onChanged: (value) {}),
+                    const Text(TTexts.rememberMe),
+                  ],
+                ),
+
+                //TOdo: Forget Password
+                TextButton(
+                    onPressed: () {}, child: const Text(TTexts.forgetPassword)),
+              ],
+            ),
+            const SizedBox(height: TSizes.spaceBtwSections),
+
+            //Todo: Sign In Button
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text(TTexts.signIn),
+              ),
+            ),
+            const SizedBox(height: TSizes.spaceBtwSections),
+
+            //Todo: Create Account Button
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text(TTexts.createAccount),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
