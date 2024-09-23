ec95dfdc2ff2c5d48788e32570503d6a3853bc97
Arman Habib <armanhabib353@gmail.com>
2024-09-23 15:16:13 +0600

    Sign Up Screen Design with separate widgets

---
+++ lib/features/autentication/screens/signup/widgets/terms_conditions_checkbox.dart
@@ -0,0 +1,54 @@
+import 'package:ecommerce/utils/constants/colors.dart';
+import 'package:ecommerce/utils/constants/sizes.dart';
+import 'package:ecommerce/utils/constants/text_strings.dart';
+import 'package:ecommerce/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+
+class TTermsAndConditionCheckbox extends StatelessWidget {
+  const TTermsAndConditionCheckbox({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+    return Row(
+      children: [
+        SizedBox(
+          width: 24,
+          height: 24,
+          child: Checkbox(value: true, onChanged: (value) {}),
+        ),
+        const SizedBox(width: TSizes.spaceBtwItems),
+        Text.rich(
+          TextSpan(
+            children: [
+              TextSpan(
+                  text: TTexts.iAgreeTo,
+                  style: Theme.of(context).textTheme.bodySmall),
+              TextSpan(
+                text: TTexts.privacyPolicy,
+                style: Theme.of(context).textTheme.bodyMedium!.apply(
+                      color: dark ? TColors.light : TColors.primary,
+                      decoration: TextDecoration.underline,
+                      decorationColor: dark ? TColors.light : TColors.primary,
+                    ),
+              ),
+              TextSpan(
+                  text: TTexts.and,
+                  style: Theme.of(context).textTheme.bodySmall),
+              TextSpan(
+                text: TTexts.termsOfUse,
+                style: Theme.of(context).textTheme.bodyMedium!.apply(
+                      color: dark ? TColors.light : TColors.primary,
+                      decoration: TextDecoration.underline,
+                      decorationColor: dark ? TColors.light : TColors.primary,
+                    ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
